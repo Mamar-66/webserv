1a16b33b45c1dfaa25fda79aceb1ccbd7665d7e3
Omar Felk <omfelk@c3r2p10.42perpignan.fr>
2025-01-10 21:57:21 +0100

    poll

--- srcs/serveur/serveur.cpp
+++ srcs/serveur/serveur.cpp
@@ -4,26 +4,47 @@
 serveur::serveur()
 {
 	this->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	std::ostringstream oss;
+
 	if (this->socket_fd == -1)
 	{
-		std::ostringstream oss;
 		oss << errno;
 		throw std::runtime_error(RED "Error create socket server\nCode error : " + oss.str() + "\nError code value : " + std::string(strerror(errno)));
 	}
-	std::cout << ORANGE "hello constructor from serveur\nfd socker servor = " << this->socket_fd << RESET <<  std::endl;
+
+	memset(&this->server_addr, 0, sizeof(this->server_addr));
+	this->server_addr.sin_family = AF_INET;
+	this->server_addr.sin_addr.s_addr = INADDR_ANY;
+	this->server_addr.sin_port = htons(PORT);
+
+	if (bind(this->socket_fd, (struct sockaddr *)&this->server_addr, sizeof(this->server_addr)) < 0)
+	{
+		oss << errno;
+		throw std::runtime_error(RED "Error bind servor\nCode error : " + oss.str() + "\nError code value : " + std::string(strerror(errno)));
+	}
+
+	if (listen(this->socket_fd, MAX_CLIENTS) < 0)
+	{
+		oss << errno;
+		throw std::runtime_error(RED "Error listen\nCode error : " + oss.str() + "\nError code value : " + std::string(strerror(errno)));
+	}
+	std::cout << ORANGE "hello constructor from serveur\n" GREEN "server listen . . .\n" RESET << std::endl;
 }
 
 serveur::~serveur()
 {
 	close(this->socket_fd);
-	std::cout << ORANGE  "destructor serveur" RESET << std::endl;
+	std::cout << ORANGE "destructor serveur" RESET << std::endl;
 }
 
-void	routine_servor()
-{	
-	try 
+void routine_servor()
+{
+	try
 	{
 		serveur server;
+		// while (true)
+		// {
+		// }
 	}
 	catch (const std::exception &e)
 	{
